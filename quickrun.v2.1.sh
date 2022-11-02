8cdcfbb89a2b5f7bbfe04ffe4127595746354dae
IPMC <IPMC@DESKTOP-TJ1LS9T.home>
2022-11-02 16:54:40 +0100

    2.1

--- quickrun.v2.1.sh
+++ quickrun.v2.1.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# need at least Java.1.8
+
+start=$SECONDS
+readonly analysisdir="${PWD}/output_dir/"
+readonly readscandir=${analysisdir}readscan/
+readonly mappingdir=${analysisdir}mapping/
+readonly umidir=${analysisdir}umis/
+readonly fastqdir=./data/fastq_pass/
+readonly tmpdir=${analysisdir}tmp/
+readonly siceloredir=${analysisdir}sicelore/
+
+mkdir $analysisdir
+mkdir $readscandir
+mkdir $umidir
+mkdir $mappingdir
+mkdir $tmpdir
+
+# need at least Java >= 12
 java=`which java`
 spoa=`which spoa`
 minimap2=`which minimap2`
 samtools=`which samtools`
-output_dir="${PWD}/output_dir"
-tmp_dir="${output_dir}/tmp/"
 
 if [ -z "$java" ] || [ -z "$spoa" ] || [ -z "$samtools" ] || [ -z "$minimap2" ]
 then
@@ -19,60 +33,20 @@ then
     exit
 fi
 
-# create output directory
-mkdir $output_dir
-mkdir $tmp_dir
+# Step 1 - Adapter search, stranding, barcode assignment to reads
+$java -jar -Xmx4G Jar/NanoporeBC_UMI_finder-2.1.jar scanfastq -d $fastqdir -o $readscandir --bcEditDistance 1 --compress 
+find ${readscandir}/passed/ -type f -name '*' | xargs pigz -dc |  pigz > ${analysisdir}fastq_pass.fastq.gz
 
-# parse illumina bam file
-$java -jar Jar/IlluminaParser-1.0.jar -i Data/190c.clta.illumina.bam -o $output_dir/190c.clta.illumina.bam.obj -t Barcodes/cellBC.190.tsv -b CB -g GN -u UB
+# Step 2 - Mapping
+$minimap2 -ax splice -uf --sam-hit-only -t 4 --junc-bed Gencode/gencode.v31.hg38.junctions.bed Data/chr4.fa.gz ${analysisdir}fastq_pass.fastq.gz | \
+   $samtools view -bS -@ 60 - | $samtools sort -m 2G -@ 4 -o ${mappingdir}passed.bam -&& $samtools index ${mappingdir}passed.bam
 
-# scan nanopore reads
-$java -jar Jar/NanoporeReadScanner-0.5.jar -i Data/190c.clta.nanopore.reads.fq -o $output_dir
+# Step 3 - UMI assignment to Nanopore SAM records
+$java -jar  -Xmx4G Jar/NanoporeBC_UMI_finder-2.1.jar assignumis --inFileNanopore ${mappingdir}passed.bam -o ${umidir}passedParsed.bam --annotationFile Gencode/gencode.v31.hg38.refFlat.txt
 
-# map reads to genome
-$minimap2 -ax splice -uf --MD --sam-hit-only -t 4 --junc-bed Gencode/gencode.v18.mm10.junctions.bed Data/chr4.fa.gz $output_dir/passed/190c.clta.nanopore.readsFWD.fq > $output_dir/minimap.sam
-$samtools view -Sb $output_dir/minimap.sam -o $output_dir/minimap.unsorted.bam
-$samtools sort $output_dir/minimap.unsorted.bam -o $output_dir/minimap.bam
-$samtools index $output_dir/minimap.bam
+# Step 4 - option a
+$java -jar -Xmx4g Jar/Sicelore-2.1.jar IsoformMatrix I=${umidir}passedParsed.bam REFFLAT=Gencode/gencode.v31.hg38.refFlat.txt CSV=${readscandir}BarcodesAssigned.tsv OUTDIR=$siceloredir PREFIX=reads VALIDATION_STRINGENCY=SILENT
 
-# tag reads with gene name
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar AddGeneNameTag I=$output_dir/minimap.bam O=$output_dir/GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE ALLOW_MULTI_GENE_READS=true USE_STRAND_INFO=true VALIDATION_STRINGENCY=SILENT
-$samtools index $output_dir/GE.bam
-
-# tag reads with fastq sequence
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar AddBamReadSequenceTag I=$output_dir/GE.bam O=$output_dir/GEUS.bam FASTQDIR=$output_dir/passed/ VALIDATION_STRINGENCY=SILENT
-$samtools index $output_dir/GEUS.bam
-
-# tag reads with cellBC/UMI barcodes
-$java -jar -Xmx4g Jar/NanoporeBC_UMI_finder-1.0.jar -i $output_dir/GEUS.bam -o $output_dir/GEUS10xAttributes.bam -k $output_dir/190c.clta.illumina.bam.obj --ncpu 8 --maxUMIfalseMatchPercent 1 --maxBCfalseMatchPercent 5 --logFile $output_dir/out.log
-$samtools index $output_dir/GEUS10xAttributes.bam
-$samtools index $output_dir/GEUS10xAttributes_umifound_.bam
-
-# generate isoform matrix
-#$java -jar -Xmx4g Jar/sicelore.v2.2.jar IsoformMatrix DELTA=2 METHOD=STRICT GENETAG=GE I=$output_dir/GEUS10xAttributes_umifound_.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt CSV=Barcodes/cellBC.190.tsv OUTDIR=$output_dir PREFIX=sicread VALIDATION_STRINGENCY=SILENT
-
-# compute consensus sequence
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar ComputeConsensus T=10 CDNATAG=US I=$output_dir/GEUS10xAttributes_umifound_.bam O=$output_dir/consensus.fq TMPDIR=$tmp_dir
-
-# map molecules to genome
-$minimap2 -ax splice -uf --MD --sam-hit-only -t 4 --junc-bed Gencode/gencode.v18.mm10.junctions.bed Data/chr4.fa.gz $output_dir/consensus.fq > $output_dir/molecule.sam
-$samtools view -Sb $output_dir/molecule.sam -o $output_dir/molecule.unsorted.bam
-$samtools sort $output_dir/molecule.unsorted.bam -o $output_dir/molecule.bam
-$samtools index $output_dir/molecule.bam
-
-# add cellBC/UMI tags
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar AddBamMoleculeTags I=$output_dir/molecule.bam O=$output_dir/molecule.tags.bam
-$samtools index $output_dir/molecule.tags.bam
-	
-# add gene name tag
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar AddGeneNameTag I=$output_dir/molecule.tags.bam O=$output_dir/molecule.tags.GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt GENETAG=GE ALLOW_MULTI_GENE_READS=true USE_STRAND_INFO=true VALIDATION_STRINGENCY=SILENT
-$samtools index $output_dir/molecule.tags.GE.bam
-	
-# generate molecule isoform matrix
-$java -jar -Xmx4g Jar/sicelore.v2.2.jar IsoformMatrix DELTA=2 METHOD=STRICT ISOBAM=true GENETAG=GE I=$output_dir/molecule.tags.GE.bam REFFLAT=Gencode/gencode.v18.mm10.refFlat.txt CSV=Barcodes/cellBC.190.tsv OUTDIR=$output_dir PREFIX=sicmol VALIDATION_STRINGENCY=SILENT
-$samtools index $output_dir/sicmol_isobam.bam
-
-# cleaning
-cd $output_dir
-#rm -fr failed 190c.clta.illumina.bam.obj consensus.fq GEUS10xAttributes_umifound_.bam GEUS10xAttributes_umifound_.bam.bai molecule.tags.GE.bam molecule.tags.GE.bam.bai GE.bam GE.bam.bai GEUS.bam GEUS.bam.bai GEUS10xAttributes.bam GEUS10xAttributes.bam.bai minimap.bam minimap.bam.bai minimap.sam minimap.unsorted.bam molecule.bam molecule.bam.bai molecule.sam molecule.tags.bam molecule.tags.bam.bai molecule.unsorted.bam out.log passed tmp
-
+end=$SECONDS
+duration=$(( end - start ))
+echo "took $duration seconds to complete"
